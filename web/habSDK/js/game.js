36b902e48c445c12848eb657d1e95f547086d6d2
Ben Oxley <ben.oxley@gmail.com>
2017-04-29 16:09:54 +0100

    refactored createSprite method and fixed a selection bug

--- web/habSDK/js/game.js
+++ web/habSDK/js/game.js
@@ -17,7 +17,7 @@ BasicGame.Boot.prototype =
             success: function(response) {
                 $.each(response, function(index, object_type) {
                     for (dir in [0, 1, 2, 3]) {
-                        game.load.image('tile', './resources/sprites/' + object_type.name + '_' + dir + '.png');
+                        game.load.image(object_type.name + '_' + dir, './resources/sprites/' + object_type.name + '_' + dir + '.png');
                     }
                 })}
             });
@@ -92,41 +92,38 @@ BasicGame.Boot.prototype =
 
             for (var xx = 0; xx < 512; xx += 20) {
                 for (var yy = 0; yy < 512; yy += 20) {
-                    // Create a tile using the new game.add.isoSprite factory method at the specified position.
-                    // The last parameter is the group you want to add it to (just like game.add.sprite)
-                    var tile = game.add.isoSprite(xx, yy, 0, 'tile', 0, isoGroup);
-                    tile.anchor.set(0.5, 0);
-                    tile.inputEnabled = true;
-                    tile.alpha = 0.4;
-                    tile.events.onInputDown.add(function(s){
-                        console.log('clicked');
-                        selectedCube = s;
-                    });
-                    selectedCube = tile;
+                    this.createNewSprite(xx,yy,0);
                 }
             }
+        },
+        createNewSprite: function(x,y,z){
+            // Create a tile using the new game.add.isoSprite factory method at the specified position.
+            // The last parameter is the group you want to add it to (just like game.add.sprite)
+            var tile = game.add.isoSprite(x, y, z, 'tile', 0, isoGroup);
+            tile.anchor.set(0.5, 0);
+            tile.inputEnabled = true;
+            tile.alpha = 0.4;
+            tile.events.onInputDown.add(function(s){
+                console.log('clicked');
+                selectedCube = s;
+            });
+            selectedCube = tile;
         },
         createMenu: function(){
             menu = new Phaser.Rectangle(1024-100, 0, 100, 768); //new Phaser.Rectangle(50,768,1024-50,0);
             //  The platforms group contains the ground and the 2 ledges we can jump on
-            menuItems = game.add.group();
+            var menuItems = game.add.group();
 
             var sprite = menuItems.create(50-10,0,'tile');
             sprite.inputEnabled = true;
-            sprite.events.onInputDown.add(function(){this.createNewSpriteCopy('tile');}, this);
+            sprite.events.onInputDown.add(function(){this.createNewSprite(0,0,5);}, this);
             //  Allow dragging - the 'true' parameter will make the sprite snap to the center
             //sprite.input.enableDrag(true);
             //menuItems.addChild(menu);
             menuItems.x = 1024-100;
         },
 
-        createNewSpriteCopy: function(origin){
-            console.log("Created");
-            var tile = game.add.isoSprite(0, 0, 5, origin, 0, isoGroup);
-            tile.anchor.set(0.5, 0);
-            selectedCube = tile;
 
-        },
         handleKeyPress: function (){
             var up = function moveUp () {
                 selectedCube.isoY -=20;
