eefde5a6084f7c8912c5659e823d0a27d283da4a
Tom <thomasjackdalby@gmail.com>
2017-04-30 00:33:54 +0100

    installed node for local running

--- analyser/layout_analyser_tests.js
+++ analyser/layout_analyser_tests.js
@@ -6,10 +6,10 @@ eval(fs.readFileSync('layout_analyser.js')+'');
 eval(fs.readFileSync('object_library.js')+'');
 eval(fs.readFileSync('data_tests.js')+'');
 
-var layout = create_test_layout();
-var result = analyse(layout);
+//var layout = create_test_layout();
+//var result = analyse(layout);
+//
+//result.log()
 
-result.log()
-
-//var obj_types = new HabObjectTypes()
-//fs.writeFile("./tmp/test_layout.json", JSON.stringify(layout))
+var obj_types = new HabObjectTypes()
+fs.writeFile("./tmp/object_types.json", JSON.stringify(obj_types))
--- analyser/object_library.js
+++ analyser/object_library.js
@@ -3,6 +3,35 @@ function HabObjectTypes() {
         new HabObjectType("bed", new Point3D(8, 3, 4), {
             "mass":10,
         }),
+
+        new HabObjectType("block1x1", new Point3D(1, 1, 1), {}),
+        new HabObjectType("block2x1", new Point3D(1, 1, 1), {}),
+        new HabObjectType("block2x2", new Point3D(1, 1, 1), {}),
+        new HabObjectType("block4x2", new Point3D(1, 1, 1), {}),
+        new HabObjectType("block4x4", new Point3D(1, 1, 1), {}),
+        new HabObjectType("block8x4", new Point3D(1, 1, 1), {}),
+
+                new HabObjectType("cooker", new Point3D(2, 2, 4), {
+            "mass":10,
+        }),
+                        new HabObjectType("kettle", new Point3D(2, 2, 4), {
+            "mass":10,
+        }),
+                        new HabObjectType("shower", new Point3D(2, 2, 4), {
+            "mass":10,
+        }),
+                                new HabObjectType("sofa", new Point3D(2, 2, 4), {
+            "mass":10,
+        }),
+                                        new HabObjectType("spectography", new Point3D(2, 2, 4), {
+            "mass":10,
+        }),
+        new HabObjectType("table-low", new Point3D(2, 2, 4), {
+            "mass":10,
+        }),
+                new HabObjectType("centrifuge", new Point3D(2, 2, 4), {
+            "mass":10,
+        }),
         new HabObjectType("cupboard", new Point3D(2, 2, 4), {
             "mass":10,
         }),
@@ -19,6 +48,12 @@ function HabObjectTypes() {
             "mass":10,
         }),
         new HabObjectType("sink", new Point3D(1, 1, 1), {
+            "mass":10,
+        }),
+                new HabObjectType("tv", new Point3D(1, 1, 1), {
+            "mass":10,
+        }),
+                new HabObjectType("worktop", new Point3D(1, 1, 1), {
             "mass":10,
         }),
         new HabObjectType("toilet", new Point3D(3, 3, 4), {
