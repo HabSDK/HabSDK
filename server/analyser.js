c17cee40e690d4d43823bcf4bc237e27dc0e3b26
Phil Crump <phil@philcrump.co.uk>
2017-04-30 01:25:45 +0000

    Update analyser module

--- server/analyser.js
+++ server/analyser.js
@@ -5,6 +5,7 @@ var sourcepath = '../analyser/';
 
 eval(fs.readFileSync(sourcepath+'spatial.js')+'');
 eval(fs.readFileSync(sourcepath+'metrics.js')+'');
+eval(fs.readFileSync(sourcepath+'rules.js')+'');
 eval(fs.readFileSync(sourcepath+'layout_analyser.js')+'');
 eval(fs.readFileSync(sourcepath+'object_library.js')+'');
 eval(fs.readFileSync(sourcepath+'data.js')+'');
